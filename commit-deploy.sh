1871f279705b9352f94762bb74c59b475f79fcab
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-05 16:19:52 -0700

    deploy

--- commit-deploy.sh
+++ commit-deploy.sh
@@ -3,4 +3,4 @@ git commit . -m 'deploy'
 git push origin master
 gcloud config set project robots-gateway
 cd src/run/orobot-cloud-run
-printf '\ry\n' | gcloud run deploy orobot-cloud-run --source ./src/run/orobot-cloud-run
+printf '\ry\n' | gcloud run deploy orobot-cloud-run --region us-central1 --source ./src/run/orobot-cloud-run
