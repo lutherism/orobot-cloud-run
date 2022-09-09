c0a4083ebec71d55b43019cdf7edb45a19c05ce3
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-09 01:11:29 -0700

    deploy

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from flask import Flask, request
 import pyassimp
 import urllib.request
 import json
+from gcloud import storage
+from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
@@ -14,13 +16,33 @@ def hello_world():
 
 @app.route("/trigger/storage-stl-create", methods = ['POST'])
 def create_object():
+    jsonBody = request.get_json()
+    selfLink = jsonBody["selfLink"]
+    id = jsonBody["id"]
+    tmpPath = "/tmp/orobot-cloud-run/in/{0}".format(id);
+    tmpExportPath = "/tmp/orobot-cloud-run/export/{0}".format(id);
     print('link: {0}'.format(json.dumps(request.get_json())))
-    #urllib.request.urlretrieve(
-        #'https://storage.googleapis.com/orobot-stls/1612589717209-battery_holder_v7.stl',
+    urllib.request.urlretrieve(
+        selfLink,
+        "/tmp/{0}".format(id))
         #"orobot-stls/1612589717209-battery_holder_v7.stl")
-    #scene = pyassimp.load("orobot-stls/1612589717209-battery_holder_v7.stl")
+    scene = pyassimp.load(tmpPath)
     print("scene")
-    #pyassimp.export(scene, "obj-output/1612589717209-battery_holder_v7.obj", "obj")
+    pyassimp.export(scene, tmpExportPath, "obj")
+    credentials_dict = {
+        'type': 'service_account',
+        'client_id': os.environ['BACKUP_CLIENT_ID'],
+        'client_email': os.environ['BACKUP_CLIENT_EMAIL'],
+        'private_key_id': os.environ['BACKUP_PRIVATE_KEY_ID'],
+        'private_key': os.environ['BACKUP_PRIVATE_KEY'],
+    }
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+        credentials_dict
+    )
+    client = storage.Client(credentials=credentials, project='robots-gateway')
+    bucket = client.get_bucket('orobot-obj')
+    blob = bucket.blob(id)
+    blob.upload_from_filename(tmpExportPath)
     return "Object created."
 
 
